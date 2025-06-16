a3ae07e9d00e08cedb199d90bc074347e49a5f9d
Rickson Simoes <rickson.simoes@hotmail.com>
2025-06-16 18:19:43 -0300

    entity framework and EFSqlLit

---
+++ ProductClient.API/Entities/Clients.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductClient.API.Entities
+{
+    public class Clients
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ ProductClient.API/Entities/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductClient.API.Entities
+{
+    public class Products
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public Guid ClientId { get; set; }
+    }
+}
